d7aed1630b85f13e1c7cbdf4ee8898243874d6ff
NadinShp <deservinglife@i.ua>
2020-12-28 14:22:12 +0200

    Реализовано решение задач ДЗ №5

---
+++ js/task-3.js
@@ -0,0 +1,40 @@
+// Напиши класс Storage, который будет создавать объекты для управления складом товаров.
+//При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+
+// Добавь методы класса:
+
+// getItems() - возвращает массив текущих товаров
+// addItem(item) - получает новый товар и добавляет его к текущим
+// removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+class Storage {
+    constructor(items) {
+        this._items = items;
+    }
+    getItems() {
+        return this._items;
+    }
+    addItem(item) {
+        if (this._items.includes(item)) return;
+        this._items.push(item);
+    }
+    removeItem(item) {
+        const removedItemIndex = this._items.indexOf(item);
+        if (removedItemIndex === -1) return;
+        this._items.splice(removedItemIndex, 1);
+    }
+ }
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
